8ea5ae85f69d673e3d3ee91812b4e5f23cbbe7f3
Isaiah Harris <harrisfambam@gmail.com>
2018-08-01 16:09:46 -1000

    query5

--- joins.sql
+++ joins.sql
@@ -6,3 +6,11 @@ WHERE poster_id = 100;
 SELECT *, users.first_name, users.last_name
 FROM posts JOIN users ON (posts.poster_id = users.id)
 WHERE users.id = 200;
+
+SELECT *, users.username
+FROM posts JOIN users ON (posts.poster_id = users.id)
+WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
+
+SELECT username 
+FROM users JOIN posts ON (users.id = posts.poster_id)
+WHERE posts.created_at > "2015-01-01 00:00:00"
