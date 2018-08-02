375ae018c478f7a9cdfda78d9854919796df6a1f
Isaiah Harris <harrisfambam@gmail.com>
2018-08-01 16:18:01 -1000

    query 7

--- joins.sql
+++ joins.sql
@@ -13,4 +13,11 @@ WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
 
 SELECT username 
 FROM users JOIN posts ON (users.id = posts.poster_id)
-WHERE posts.created_at > "2015-01-01 00:00:00"
+WHERE posts.created_at > "2015-01-01 00:00:00";
+
+SELECT posts.title, posts.content, users.username 
+FROM posts JOIN users ON (users.id = posts.poster_id)
+WHERE users.created_at < "2015-01-01 00:00:00";
+
+SELECT posts.post_title AS 'Post Title', *
+FROM comments JOIN posts ON (comments.post_id = posts.post_id);
